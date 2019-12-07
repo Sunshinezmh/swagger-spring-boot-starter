c8a494d6fa2095115af7ed1da1a8ee148ea9aeee
Sunshinezmh <18333640045@163.com>
2019-12-07 19:44:38 +0800

    pom升级版本-sunshine-2019年12月06日20:49:18

--- src/main/java/com/github/sunshinezmh/swaggerspringbootstarter/SwaggerSpringBootStarterApplication.java
+++ src/main/java/com/github/sunshinezmh/swaggerspringbootstarter/SwaggerSpringBootStarterApplication.java
@@ -1,4 +1,4 @@
-package com.dmsdbj.sunshinezmh.swaggerspringbootstarter;
+package com.github.sunshinezmh.swaggerspringbootstarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
